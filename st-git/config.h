b150af3b7a46632dc9cb1bb6d60672b293621e7d
jkajan <j@kajan.io>
2018-03-20 13:01:20 +0200

    Bigger border for st-git

--- st-git/config.h
+++ st-git/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Gohu GohuFont:pixelsize=14:autohint=true";
-static int borderpx = 10;
+static int borderpx = 30;
 
 /*
  * What program is execed by st depends of these precedence rules:
