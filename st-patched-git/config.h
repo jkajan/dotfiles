13276fc8e18813dbf8b7408148c8616e5b3b93e4
jkajan <j@kajan.io>
2018-05-07 15:59:31 +0300

    Add files for st-patched-git, set as default

--- st-patched-git/config.h
+++ st-patched-git/config.h
@@ -108,25 +108,7 @@ static unsigned int cursorshape = 4;
 *
 */
 
-static const char *colorname[] = {
-  "#534636", /* base01 */
-  "#cb6077", /* base08 */
-  "#beb55b", /* base0B */
-  "#f4bc87", /* base0A */
-  "#8ab3b5", /* base0D */
-  "#bb9584", /* base0F */
-  "#7bbda4", /* base0C */
-  "#e9e1dd", /* base06 */
-  "#3b3228", /* base00 */
-  "#d28b71", /* base09 */
-  "#645240", /* base02 */
-  "#7e705a", /* base03 */
-  "#b8afad", /* base04 */
-  "#a89bb9", /* base0E */
-  "#d0c8c6", /* base05 */
-  "#f5eeeb", /* base07 */
-};
-
+#include "/home/taate/.cache/wal/colors-wal-st.h"
 /*
 * Default colors (colorname index)
 * foreground, background, cursor, reverse
