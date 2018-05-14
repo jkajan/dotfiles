24997a83c41bd26737babab581985c3e079666d3
jkajan <j@kajan.io>
2018-05-14 18:45:21 +0300

    Add bspc commands to wal script

--- .config/wal/postwal.sh
+++ .config/wal/postwal.sh
@@ -3,3 +3,10 @@
 echo "Setting background"
 xsetroot -solid $(cat /home/taate/.cache/wal/colors | head -1)
 
+. "${HOME}/.cache/wal/colors.sh"
+
+bspc config normal_border_color "$color0"
+bspc config active_border_color "$color1"
+bspc config focused_border_color "$color15"
+bspc config presel_border_color "$color1"
+
