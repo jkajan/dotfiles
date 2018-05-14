3ce00d60b286627a3cc239ba2dba20d731cd1cd9
jkajan <j@kajan.io>
2018-05-14 19:12:13 +0300

    Fix bspwm presel color

--- .config/wal/postwal.sh
+++ .config/wal/postwal.sh
@@ -8,5 +8,5 @@ xsetroot -solid $(cat /home/taate/.cache/wal/colors | head -1)
 bspc config normal_border_color "$color0"
 bspc config active_border_color "$color1"
 bspc config focused_border_color "$color15"
-bspc config presel_border_color "$color1"
+bspc config presel_feedback_color "$color1"
 
