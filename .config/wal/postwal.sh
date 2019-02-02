6793426c751eee97129a0f13ba6fa2c6d6bf20b3
jkajan <jkajan@github.com>
2019-02-02 12:42:32 +0200

    Changes

--- .config/wal/postwal.sh
+++ .config/wal/postwal.sh
@@ -11,3 +11,6 @@ bspc config focused_border_color "$color15"
 bspc config presel_feedback_color "$color1"
 
 $HOME/.config/wal/qutebrowser_reload.py 
+oomox-cli $HOME/.cache/wal/colors-oomox
+oomox-archdroid-icons-cli $HOME/.cache/wal/colors-oomox
+$HOME/.telegram-palette-gen/telegram-palette-gen --palette $HOME/.cache/wal/colors.sh
