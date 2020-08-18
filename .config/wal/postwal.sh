4b79f345fe473a3dd1cd6049c6289550286e1c9a
jkajan <jkajan@github.com>
2020-08-18 17:05:50 +0300

    Set background

--- .config/wal/postwal.sh
+++ .config/wal/postwal.sh
@@ -11,8 +11,6 @@ $HOME/.config/wal/qutebrowser_reload.py
 oomox-cli $HOME/.cache/wal/colors-oomox
 oomox-archdroid-icons-cli $HOME/.cache/wal/colors-oomox
 $HOME/.telegram-palette-gen/telegram-palette-gen --palette $HOME/.cache/wal/colors.sh
-cp $HOME/.cache/wal/pscircle.service $HOME/.config/systemd/user/
-systemctl --user daemon-reload
-systemctl --user restart pscircle.service
+hsetroot -solid "$color0"
 pkill dunst 
 dunst &
