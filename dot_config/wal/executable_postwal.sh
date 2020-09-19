8cca07009990505ce78c883834653bdfe7d56714
Jonatan Kaján <j@kajan.io>
2020-09-19 16:00:28 +0300

    Switch to chezmoi for managing files

---
+++ dot_config/wal/executable_postwal.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+. "${HOME}/.cache/wal/colors.sh"
+
+bspc config normal_border_color "$color0"
+bspc config active_border_color "$color1"
+bspc config focused_border_color "$color15"
+bspc config presel_feedback_color "$color1"
+
+hsetroot -solid "$color0"
+
+$HOME/.config/wal/qutebrowser_reload.py 
+oomox-cli $HOME/.cache/wal/colors-oomox
+oomox-archdroid-icons-cli $HOME/.cache/wal/colors-oomox
+$HOME/.telegram-palette-gen/telegram-palette-gen --theme '$HOME/.cache/wal/colors.sh'
+pkill dunst 
+dunst &
